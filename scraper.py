c9be42f4c9b2411b7c665a77ee7c5a68359be4f8
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-18 14:56:48 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -78,10 +78,10 @@ step = 1
 	
 for seq in range(start, end, step):
 	# Skip already scraped entries
-	tmp = scraperwiki.sqlite.select("seq, month from swdata where seq = '%d'" % seq)
-	if 0 < len(tmp):
-        	print "skipping %d already scraped, month %s" % (seq, tmp[0]['month'])
-	continue
+	#tmp = scraperwiki.sqlite.select("seq, month from swdata where seq = '%d'" % seq)
+	#if 0 < len(tmp):
+        #	print "skipping %d already scraped, month %s" % (seq, tmp[0]['month'])
+	#continue
         if fetch_doffin_entry(curmon, seq, datastore):
             if 0 == len(datastore) % 10:
                 scraperwiki.sqlite.save(unique_keys=['month', 'seq'], data=datastore)
